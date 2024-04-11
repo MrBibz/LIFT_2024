8190caefcc1e69b204b4aef63b738f31ea549609
IulianBlyat <iulianghitiu0102@gmail.com>
2024-04-11 13:56:57 -0400

    add maven spring API

---
+++ liftAPP/src/main/java/com/lift/liftAPP/LiftApiApplication.java
@@ -0,0 +1,13 @@
+package com.lift.liftAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftApiApplication.class, args);
+	}
+
+}
---
+++ liftAPP/src/test/java/com/lift/liftAPP/LiftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lift.liftAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
